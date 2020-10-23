35010be20fa1b456d39700e5889f8093d7d91b2c
itsnou <emiliano.meneses@gmail.com>
2020-10-23 20:07:09 -0300

    update

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '8ac4a070129b4a12ba9309639b92d6ac'
+});
+
+const handleApiCall = (req,res) => {
+    app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err=> res.stats(400).json('No se logro trabajar con la API'));
+}
+  
+
 const handleImage = (req,res,db)=>{
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +25,6 @@ const handleImage = (req,res,db)=>{
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const knex = require('knex');
 const register = require ('./controllers/register');
 const signin = require ('./controllers/signin');
 const id = require ('./controllers/id');
-const image = require ('./controllers/image')
+const image = require ('./controllers/image');
 
 const db = knex({
     client: 'pg',
     connection: {
         host: '127.0.0.1',
-        user: 'user',
-        password: 'password',
+        user: 'itsnou',
+        password: 'elemivl900',
         database: 'smart-brain'
     }
 });
@@ -31,6 +31,8 @@ app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)})
 app.post('/register', (req,res) => { register.handleRegister(req,res,db,bcrypt)})
 app.get('/profile/:id', (req,res) => {id.handleId(req,res,db)})
 app.put('/image', (req,res) => {image.handleImage(req,res,db)})
+app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)})
+
 app.listen(3000,()=>{
     console.log('funciona')
 })
